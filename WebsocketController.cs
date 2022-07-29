4db83124fb64e30265ed33c65e968ff973023c0f
John caleb <Calebjohn3112@gmail.com>
2022-07-29 14:22:14 +0100

    Add files via upload

---
+++ WebsocketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CHATFREE.Controllers
+{
+    [AllowAnonymous]
+    public class WebsocketController : Controller
+    {
+       public async Task Connect() {
+       
+            if(HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                var Websockets = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            }
+       }         
+    }
+}
