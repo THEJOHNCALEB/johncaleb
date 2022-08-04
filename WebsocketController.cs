1d66d3e236d4be22350cf942f64a7145ffa4505a
John caleb <Calebjohn3112@gmail.com>
2022-08-04 15:38:07 +0100

    Update WebsocketController.cs

--- WebsocketController.cs
+++ WebsocketController.cs
@@ -5,8 +5,8 @@ namespace CHATFREE.Controllers
     [AllowAnonymous]
     public class WebsocketController : Controller
     {
-       public async Task Connect() {
-       
+
+       public async Task Connect(){
             if(HttpContext.WebSockets.IsWebSocketRequest)
             {
                 var Websockets = await HttpContext.WebSockets.AcceptWebSocketAsync();
